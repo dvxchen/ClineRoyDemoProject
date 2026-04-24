2db89851f403364e8c48ecd50dc7025d46e02a1c
Chen <davy.chen@sap.com>
2026-04-24 23:08:50 +0800

    email heml

--- email.js
+++ email.js
@@ -27,13 +27,76 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+
+// 1. 读取 JSON 文件
+const filePath = path.join(__dirname, 'merged-logs.json');
+const rawData = fs.readFileSync(filePath, 'utf8');
+const users = JSON.parse(rawData);
+
+const htmlContent = `
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <title>用户列表</title>
+    <style>
+        table { width: 100%; border-collapse: collapse; margin-top: 20px; }
+        th, td { border: 1px solid #ddd; padding: 12px; text-align: left; }
+        th { background-color: #f4f4f4; }
+        tr:nth-child(even) { background-color: #f9f9f9; }
+    </style>
+</head>
+<body>
+    <h1>Log</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>index</th>
+                <th>title</th>
+                <th>ts</th>
+                <th>event</th>
+                <th>row</th>
+                <th>step</th>
+                <th>url</th>
+                <th>sttus</th>
+                <th>value</th>
+                <th>expected</th>
+                <th>startAt</th>
+                <th>note</th>
+            </tr>
+        </thead>
+        <tbody>
+            ${users.map(user => `
+                <tr>
+                    <td>${user.index}</td>
+                    <td>${user.title}</td>
+                    <td>${user.ts}</td>
+                    <td>${user.event}</td>
+                    <td>${user.row}</td>
+                    <td>${user.step}</td>
+                    <td>${user.url}</td>
+                    <td>${user.status}</td>
+                    <td>${user.value}</td>
+                    <td>${user.expected}</td>
+                    <td>${user.startedAt}</td>
+                    <td>${user.note}</td>
+                </tr>
+            `).join('')}
+        </tbody>
+    </table>
+</body>
+</html>
+`;
+
+
+
 // 3. 定义邮件内容
 const mailOptions = {
     from: '"系统日志机器人" <dvxchen@qq.com>', // 发件人
     to: "davy.chen@sap.com",        // 收件人
     subject: "当前目录日志报告",     // 邮件主题
     text: "请查看附件中的日志文件。", // 纯文本正文
-    html: "<b>你好，</b><p>这是自动发送的日志报告，请查收附件。</p>", // HTML 正文
+    html: htmlContent, // HTML 正文
     attachments: [
         {
             filename: 'merged-logs.json',       // 附件文件名
