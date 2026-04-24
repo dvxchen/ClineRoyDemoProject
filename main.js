f310db4d5beaecab942b7b4c8e0821610e33ffac
Chen <davy.chen@sap.com>
2026-04-24 21:38:53 +0800

    fix merged jaon

--- main.js
+++ main.js
@@ -46,80 +46,85 @@ let allLogs = [];
       const jsFiles = files.filter(file => file.endsWith('.js'));
       // 使用 for...of 循环
       for (const file of jsFiles) {
-        const filePath = path.join(dirPath, file);
-        try {
+        try {  // read js in current folder
+
+          const filePath = path.join(dirPath, file);
           const content = await fs.readFile(filePath, 'utf8');
-          console.log(`读取完成: ${file}`);
+          console.log(`js content read ok: ${file}`);
+        } catch (err) {
+          console.error('read js file error:', err);
+        }
+        const dirPath = path.join(__dirname, file0, '\\');
+        //delete  log.json file in parent folder and current folder
+        const filePPath = path.join(__dirname, 'log.json');
+        const filePPathAll = path.join(__dirname, 'merged-logs.json');
+        const fileCPath = path.join(__dirname, file0, 'log.json');
 
-          const dirPath = path.join(__dirname, file0, '\\');
+        try { // delete log files
 
-          //delete  log.json file in parent folder and current folder
+          await fs2.remove(filePPathAll);
+          await fs2.remove(filePPath);
+          await fs2.remove(fileCPath);
+          console.log('清理完成（clear if exist or not）');
 
-          const filePPath = path.join(__dirname, 'log.json');
-          const filePPathAll = path.join(__dirname, 'merged-logs.json');
-          const fileCPath = path.join(__dirname, file0, 'log.json');
+        } catch (err) {
+          console.error('remove log file error:', err);
+        }
 
-          try {
-            await fs2.remove(filePPathAll);
-            await fs2.remove(filePPath);
-            await fs2.remove(fileCPath);
-            console.log('清理完成（无论文件是否存在）');
-          } catch (err) {
-            console.error('发生错误:', err);
-          }
+        try {
+          await runFile(dirPath + '\\' + file);
+        } catch (err) {
+          console.error('run  js error:', err);
+        }
 
-          await runFile(dirPath + file);
 
-          let dataJson = [];
+        let dataJson = [];
 
-          try {
-            dataJson = fs3.readFileSync(fileCPath, 'utf-8');
-            console.log('读取内容:', dataJson);
-          } catch (err) {
-            if (err.code === 'ENOENT') {
-              console.log('文件不存在当前目录');
-              try {
-                dataJson = fs3.readFileSync(filePPath, 'utf-8');
-                console.log('读取内容:', dataJson);
-              } catch (err) {
-                if (err.code === 'ENOENT') {
-                  console.log('文件不存在根目录');
-                }
+        try { // find log.json file 
+          dataJson = fs3.readFileSync(fileCPath, 'utf-8');
+          //console.log('读取内容:', dataJson);
+        } catch (err) {
+          if (err.code === 'ENOENT') {
+            console.log('文件不存在当前目录');
+            try {
+              dataJson = fs3.readFileSync(filePPath, 'utf-8');
+              //console.log('读取内容:', dataJson);
+            } catch (err) {
+              if (err.code === 'ENOENT') {
+                console.log('文件不存在根目录');
               }
-
             }
-          }
 
-          //     const content1 = fs1.readFileSync(dirPath + 'log.json', 'utf8');
-          // 假设每行是一个 JSON 对象
-          //     const lines = content1.split('\n').filter(line => line.trim());
-          //      const jsonLines = lines.map(line => JSON.parse(line));
+          }
+        }
+        try {
           allLogs = allLogs.concat(dataJson);
 
-
-
-
         } catch (readErr) {
-          console.error(`读取文件 ${file} 失败:`, readErr);
+          console.error(`concatenate error 1: `, readErr);
         }
+
+
       }
     }
-    try {
-      // 3. 写入合并后的文件 (格式化为 JSON 数组)
-      const filePPathAllx = path.join(__dirname, 'merged-logs.json');
+
+    try { //write log.json to merged-logs.json
+      const filePPathAllx = path.join(__dirname, '\\', 'merged-logs.json');
       fs1.writeFileSync(filePPathAllx, JSON.stringify(allLogs, null, 2));
       console.log('JSON 日志合并完成');
-
-      await runFile(path.join(__dirname, 'email.js'));
-
     } catch (err) {
-      console.error('读取合并文件失败:', err);
+      console.error('cancategate error 2:', err);
     }
 
+    try { // send email
+      await runFile(path.join(__dirname, 'email.js'));
+    } catch (err) {
+      console.error('cancategate error:', err);
+    }
 
 
   } catch (err) {
-    console.error('读取目录失败:', err);
+    console.error('unknown error:', err);
   }
 })();
 
